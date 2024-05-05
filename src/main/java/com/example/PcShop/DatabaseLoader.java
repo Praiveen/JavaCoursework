f0290ff65d9e73a30e5a6f9f38aaf3e0e3526af1
Ivan_Karagachev <ivan.karagachev@gmail.com>
2024-05-05 14:57:32 +0300

    main

--- src/main/java/com/example/PcShop/DatabaseLoader.java
+++ src/main/java/com/example/PcShop/DatabaseLoader.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
